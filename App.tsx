91b325804f7fe64aed28ed32cb24714ddbb5f036
MinhQuan.dev <mquan592003@gmail.com>
2024-11-26 00:32:13 +0700

    Logic Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { LoginScreen, SplashScreen } from './src/screens'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
+  const [isShownSplashScreen, setIsShownSplashScreen] = useState(true)
+
+  useEffect(() => {
+    // ham tao
+    const timeout = setTimeout(() => {
+      setIsShownSplashScreen(false)
+    },1500)
+    // ham huy
+    return () => clearTimeout(timeout);
+  },[]) // chi chay 1 lan
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    isShownSplashScreen ? <SplashScreen/> : <NavigationContainer>
+      <LoginScreen/>
+    </NavigationContainer> 
   )
 }
 
