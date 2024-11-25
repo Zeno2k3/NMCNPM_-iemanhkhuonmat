6f62b69e47c7486170d82da8dd7c45ac6b28ea7c
MinhQuan.dev <mquan592003@gmail.com>
2024-11-26 00:19:55 +0700

    setup App

---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+import LoginScreen from "./auth/LoginScreen"
+import HomeScreen from "./home/HomeScreen"
+import SplashScreen from "./SplashScreen"
+
+export {
+    HomeScreen, SplashScreen,LoginScreen
+}
